55310b9408420855433ea787c6063f1fd1d18aa8
VikashLoomba <loomba.vikash@gmail.com>
2025-05-12 00:48:04 -0700

    chore: update version to 0.0.41 and comment out update notification

--- src/extension.ts
+++ src/extension.ts
@@ -67,7 +67,7 @@ export async function activate(context: vscode.ExtensionContext) {
 	);
 
 	// Show "What's New" page if the extension has been updated
-	await showUpdatesToUser(context);
+	// await showUpdatesToUser(context);
 }
 
 // This method is called when your extension is deactivated
